fb4ac4f3608ac38b61e4e8825704c36a9acfeb24
Penumudi Datta Jagan Mohan <139993968+Dattajagan@users.noreply.github.com>
2025-05-22 21:29:44 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Pair with given sum in a sorted array/pair-with-given-sum-in-a-sorted-array.java
@@ -0,0 +1,44 @@
+// User function Template for Java
+
+class Solution {
+
+    int countPairs(int arr[], int target) {
+        int count = 0;
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+
+            if (sum == target) {
+                if (arr[left] == arr[right]) {
+                    int n = right - left + 1;
+                    count += (n * (n - 1)) / 2;
+                    break;
+                } else {
+                    int countLeft = 1, countRight = 1;
+
+                    while (left + 1 < right && arr[left] == arr[left + 1]) {
+                        countLeft++;
+                        left++;
+                    }
+
+                    while (right - 1 > left && arr[right] == arr[right - 1]) {
+                        countRight++;
+                        right--;
+                    }
+
+                    count += countLeft * countRight;
+                    left++;
+                    right--;
+                }
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return count;
+    }
+}
+
